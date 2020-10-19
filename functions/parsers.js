e9f8070e0f622b32f70e29a5528cd4767e75f50e
listmanster <listmanster@gmail.com>
2020-10-19 16:20:43 +0530

    Updating page

---
+++ functions/parsers.js
@@ -0,0 +1,8 @@
+const parsers = require("./parsers.json");
+
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(parsers)
+    };
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,20 @@
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 
 import {Helmet} from 'react-helmet';
-import {Router} from '@reach/router';
-
-import {IdentityContext } from '../../identity-context';
-
+//import {IdentityContext } from '../../identity-context';
 import Layout from '../components/layout';
-import Home from '../components/home';
-//import PrivateRoute from '../components/private-route';
-import {Login, SignUp} from '../components/login';
 import { navigate } from 'gatsby';
 
 
 
 export default () => {
-    const {user, identity: netlifyIdentity} = useContext(IdentityContext);
+    //const {user, identity: netlifyIdentity} = useContext(IdentityContext);
     return (
         <Layout>
             <Helmet>
                 <title>Parserbase - parsing as a service</title>
             </Helmet>
             {navigate('/app', {replace: true})}
-            
-    {/*             <PrivateRoute path="/" component={Home} /> */ }
-                <button onClick={ () => netlifyIdentity.open() } >Log In </button>
-{/*             <Router>            
-                <Login path="/login"  />
-                <SignUp path="/signup" />
-            </Router>
- */}         </Layout>
+        </Layout>
     );
 } 
