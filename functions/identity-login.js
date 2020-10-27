e04171952f110c287e4e8375e2df57436544bdee
listmanster <listmanster@gmail.com>
2020-10-27 17:57:11 +0530

    Updating identity login

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,7 +1,11 @@
+const sendQuery = require('./utils/sendquery');
+const queries = require("./utils/queries");
+
+const GET_USER_INFO = queries.GET_USER_INFO;
 
 
-/* 
-const fetchUserInfo = async () => {
+
+/* const fetchUserInfo = async () => {
   const data = await secureQuery(netlifyIdentity, '/api/userDetails', {})
   return data;  
 };
@@ -12,18 +16,16 @@ const fetchUserData = async (userId, userEmail) => {
   const userInfoData = await fetchUserInfo(userId, userEmail);
   const extendedData = userInfoData.data.findUserInfoByAuthUser.data;
   const isUserRegistered = extendedData.length > 0  ; 
-  if (isUserRegistered) {
-      setAuthToken(extendedData[0].authToken);
-      setUserRegistered(true);
-  } else {
-      setUserRegistered(false);
-  }
-};
- */
+  return isUserRegistered;
+}; */
+ 
 exports.handler = async function(event, context, callback) {
     const data = JSON.parse(event.body);
     const { user } = data;
-    const {email, id} = user ; 
+    const {clientContext, identity} = context;
+    const {email, id, token} = user ; 
+
+    //const registered = await fetchUserData(id, email);
       
     const responseBody = {
       app_metadata: {
@@ -31,8 +33,7 @@ exports.handler = async function(event, context, callback) {
       },
       user_metadata: {
         ...user.user_metadata, // append current user metadata
-        special: JSON.stringify(Object.getOwnPropertyNames(data)),
-        special2: JSON.stringify(Object.getOwnPropertyNames(context))
+        special: token + "~" + email
       }
     };
     callback(null, {
